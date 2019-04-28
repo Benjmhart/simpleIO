8529714f6e75ce0dbdfce96aad6cb1b3906223e9
benjmhart <benjmhart@gmail.com>
2019-04-28 00:17:05 -0400

    adjusted parse

--- src/SimpleIO.hs
+++ src/SimpleIO.hs
@@ -51,7 +51,7 @@ parseEvent str = Just $ Event uname time method path
   getUnameStr = T.filter (/= ' ') . T.takeWhile (/= '[')
   getMethodStr =
     T.takeWhile (isAlpha) . T.dropWhile (== ' ') . drop 1 . T.dropWhile (/= ']')
-  getTimeStr = T.takeWhile (/= ']') . T.drop 1 . T.drop (T.length uname)
+  getTimeStr = T.takeWhile isNumber . T.dropWhile (not . isNumber) . T.dropWhile (/=' ')
   getPath    = T.dropWhile (/= '/')
   uname      = getUnameStr str
   time       = fromMaybe 0 $ readMay . getTimeStr $ str
