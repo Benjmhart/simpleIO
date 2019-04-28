3d735264ed613c5c2818b2c6f8c99183deaa903d
benjmhart <benjmhart@gmail.com>
2019-04-28 00:03:21 -0400

    basic exercise

--- src/SimpleIO.hs
+++ src/SimpleIO.hs
@@ -3,37 +3,9 @@
 
 module SimpleIO where
 
-import           ClassyPrelude                  ( putStrLn
-                                                , IO
-                                                , print
-                                                , getArgs
-                                                , ($)
-                                                , (.)
-                                                , FilePath
-                                                , Maybe(..)
-                                                , Either(..)
-                                                , (<>)
-                                                , tshow
-                                                , readFileUtf8
-                                                , return
-                                                , unpack
-                                                , tryIOError
-                                                , listToMaybe
-                                                , drop
-                                                , show
-                                                , readMay
-                                                , fromMaybe
-                                                , Text
-                                                , Eq
-                                                , Show
-                                                , Read
-                                                , Int
-                                                , (/=)
-                                                , (==)
-                                                )
+import           ClassyPrelude
 import qualified Data.Text                     as T
 import           Data.Char                      ( isAlpha )
-
 
 simpleIOMain :: IO ()
 simpleIOMain = do
@@ -47,17 +19,32 @@ simpleIOMain = do
       case fileContents of
         Left  e -> print $ "Error: " <> tshow e
         Right c -> do
-             -- split file into lines and get rid of the header row
-          let rows = drop 1 $ T.lines c -- drop is safe, tail is not
-          -- we need to map a function over each row to parse it into our types
-          return ()
+          let rows   = drop 1 $ T.lines c
+              events = catMaybes $ parseEvent <$> rows
+          case events of
+            [] -> print "No Events in log!"
+            xs -> do
+              let problem = findBefore xs
+              case problem of
+                Nothing -> putStrLn "Ain't no problem!"
+                Just p  -> do
+                  print $ "the problem is: " <> tshow p
+                  return ()
 
- -- test with ghci
+findBefore :: [Event] -> Maybe Event
+findBefore []                           = Nothing
+findBefore (x : (Event _ _ ERR _) : _ ) = Just x
+findBefore (x                     : xs) = findBefore xs
 
 
-data Method = POST | Err deriving (Eq, Show, Read)
+-- t' x = trace ("\nmethod string: " <> (T.unpack $ x) <> "\n") x
 
-data Event = Event Text Int Method Text deriving (Eq, Show, Read)
+data Method = POST | GET | ERR deriving (Eq, Show, Read)
+
+data Event = Event Text Int Method Text deriving (Eq)
+
+instance Show Event where 
+  show (Event u t m p) = "Event " <> T.unpack u <> " " <> show t <> " " <> show m <> " " <> T.unpack p   
 
 parseEvent :: Text -> Maybe Event
 parseEvent ""  = Nothing
@@ -65,11 +52,11 @@ parseEvent str = Just $ Event uname time method path
  where
   getUnameStr = T.filter (/= ' ') . T.takeWhile (/= '[')
   getMethodStr =
-    T.takeWhile (isAlpha) . T.dropWhile (/= ' ') . T.dropWhile (/= ']')
-  getTimeStr  = T.takeWhile (/= ']') . T.drop 1 . T.drop (T.length uname)
-  getPath = T.dropWhile (/= '/')
-  uname       = getUnameStr str
-  time        = fromMaybe 0 $ readMay . getTimeStr $ str
-  method  = fromMaybe POST $ readMay . getMethodStr $ str
-  path    = getPath str
+    T.takeWhile (isAlpha) . T.dropWhile (== ' ') . drop 1 . T.dropWhile (/= ']')
+  getTimeStr = T.takeWhile (/= ']') . T.drop 1 . T.drop (T.length uname)
+  getPath    = T.dropWhile (/= '/')
+  uname      = getUnameStr str
+  time       = fromMaybe 0 $ readMay . getTimeStr $ str
+  method     = fromMaybe POST $ readMay . getMethodStr $ str
+  path       = getPath str
 
