7d18c855fa682ccc5a6d990995bf9f38f38a0818
benjmhart <benjmhart@gmail.com>
2019-04-27 20:37:26 -0400

    added comments

--- src/SimpleIO.hs
+++ src/SimpleIO.hs
@@ -7,7 +7,10 @@ import           ClassyPrelude                  ( putStrLn
                                                 , IO
                                                 )
 
+
+
+ -- () - pronounced 'Unit'
 simpleIOMain :: IO ()
 simpleIOMain = putStrLn "Welcome to the Haskell Beginners class Exercise"
-
+  -- putStrLn, takes a string, outputs it to the terminal respecting newlines, and returns ()
 
