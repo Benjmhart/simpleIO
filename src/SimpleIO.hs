80fa0fd5d9cfd5100c47bcc06814ae6d9b7b2f1f
benjmhart <benjmhart@gmail.com>
2019-05-01 19:13:27 -0400

    removing Filepath

--- src/SimpleIO.hs
+++ src/SimpleIO.hs
@@ -9,7 +9,6 @@ import           ClassyPrelude                  ( putStrLn
                                                 , getArgs
                                                 , ($)
                                                 , (.)
-                                                , FilePath
                                                 , listToMaybe
                                                 )
 
